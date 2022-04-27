20357acd740b9470808d0929588356d090d7063d
Esther O <64683541+esther-cs@users.noreply.github.com>
2022-04-27 11:51:09 +0800

    Add files via upload

---
+++ T2_Ex3/T2_Ex3_3E_22_usemod.py
@@ -0,0 +1,11 @@
+import T2_Ex3_3E_22_mod as mylib
+f = open("userdata.txt", "r")
+alldata = [tuple([int(i) for i in j.split(',')]) for j in f.read().split('\n') if len(j) > 0]
+f.close()
+mylib.sortTupleList(alldata,0)
+print("The upper quartile User ID data is "+ mylib.prettyPrint(alldata[len(alldata)//4]))
+mylib.sortTupleList(alldata,1)
+print("The upper quartile Age data is "+ mylib.prettyPrint(alldata[len(alldata)//4]))
+mylib.sortTupleList(alldata,2)
+print("The upper quartile Height data is "+ mylib.prettyPrint(alldata[len(alldata)//4]))
+
